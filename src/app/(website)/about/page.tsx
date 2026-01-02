335295d7fbd009e5ddff566ec03d28a05274532b
MiledAmer <midou.slm8@gmail.com>
2026-01-02 17:10:30 +0100

    delete unused vars

--- src/app/(website)/about/page.tsx
+++ src/app/(website)/about/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 import { Heart, Leaf, Users } from "lucide-react";
 import { useLocale, useTranslations } from "next-intl";
 
