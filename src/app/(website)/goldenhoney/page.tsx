388c5aec2e242b1445b23c42afcd860ead2dc03a
MiledAmer <midou.slm8@gmail.com>
2026-01-10 20:35:12 +0100

    working on easter egg

---
+++ src/app/(website)/goldenhoney/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import SplashCursor from "@/components/SplashCursor";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <SplashCursor />
+      hello
+    </div>
+  );
+}
