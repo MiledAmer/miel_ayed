530ed950cae9ed1e43c862476ce621d462b8a201
MiledAmer <midou.slm8@gmail.com>
2026-01-10 14:04:35 +0100

    disable dark mode

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,12 +1,11 @@
-// import { Analytics } from '@vercel/analytics/next'
 import "../globals.css";
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { getCategoriesWithSubcategories } from "@/sanity/sanity-utils";
 import { NextIntlClientProvider } from "next-intl";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 export default async function RootLayout({
   children,
@@ -15,29 +14,29 @@ export default async function RootLayout({
 }>) {
   const categories = await getCategoriesWithSubcategories();
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange={false}
-    >
-      <NextIntlClientProvider>
-        <Header categories={categories} />
-        {children}
-        <Footer />
-        <Toaster
-          position="top-right"
-          style={
-            {
-              "--normal-bg": "var(--accent)",
-              "--normal-text": "var(--accent-foreground)",
-              "--normal-border": "var(--accent)",
-            } as React.CSSProperties
-          }
-        />
-      </NextIntlClientProvider>
-
+    // <ThemeProvider
+    //   attribute="class"
+    //   defaultTheme="system"
+    //   enableSystem
+    //   disableTransitionOnChange={false}
+    // >
+    <NextIntlClientProvider>
+      <Header categories={categories} />
+      {children}
+      <Footer />
+      <Toaster
+        position="top-right"
+        style={
+          {
+            "--normal-bg": "var(--accent)",
+            "--normal-text": "var(--accent-foreground)",
+            "--normal-border": "var(--accent)",
+          } as React.CSSProperties
+        }
+      />
       <Analytics />
-    </ThemeProvider>
+    </NextIntlClientProvider>
+
+    // </ThemeProvider>
   );
 }
